6803ece6ab849cd390807edda3a71e338a9a066a
marazmatikpepe-coder <marazmatikpepe@gmail.com>
2026-05-23 13:18:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -54,3 +54,16 @@ self.addEventListener('activate', event => {
     );
     self.clients.claim();
 });
+// Обработка кликов по уведомлениям
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true })
+            .then(function(clientList) {
+                if (clientList.length > 0) {
+                    return clientList[0].focus();
+                }
+                return clients.openWindow('/');
+            })
+    );
+});
